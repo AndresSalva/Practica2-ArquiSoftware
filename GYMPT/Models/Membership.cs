de294cab851ba749ec29d1eaf824e22d65418d8b
Mateo Gandarillas <mateoganav@gmail.com>
2025-09-28 11:30:45 -0400

    feat: Model membership

---
+++ GYMPT/Models/Membership.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GYMPT.Models
+{
+    public class Membership
+    {
+        public long Id { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? LastModification { get; set; }
+
+        public bool? IsActive { get; set; }
+
+        [MaxLength(255)]
+        public string? Name { get; set; }
+
+        public float? Price { get; set; }
+
+        public string? Description { get; set; }
+
+        public long? MonthlySessions { get; set; }
+    }
+}
