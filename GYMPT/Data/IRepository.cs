2fec37e14039e3dd1ee6fcdac7ce5d76956a805f
gehre <huascar.rivero@ucb.edu.bo>
2025-09-29 22:47:07 -0400

    refactor: actualizacion de interfaz para Create, Update, Delete

--- GYMPT/Data/IRepository.cs
+++ GYMPT/Data/IRepository.cs
@@ -6,5 +6,11 @@ namespace GYMPT.Data
     public interface IRepository<T> where T : BaseModel
     {
         Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> CreateAsync(T entity);
+
+        Task<T> UpdateAsync(T entity);
+
+        Task<bool> DeleteByIdAsync(int id); // El delete solo es logico, se debe hacer un update que modifique solamente el atributo is_active a false
     }
 }
