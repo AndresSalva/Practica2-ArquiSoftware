fd967463178a78b6a04383363513b21406a5dc5a
Jose <jose.lopez.s@ucb.edu.bo>
2025-10-12 22:33:40 -0400

    fix: correccion en compilacion

--- GYMPT/obj/Debug/net8.0/GYMPT.AssemblyInfo.cs
+++ GYMPT/obj/Debug/net8.0/GYMPT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYMPT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0af36cfd7a2c71fb1d4e4f35265e2e5df73bc43f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77bd69f543ee0ff8a71f8dc618eea07df0d6e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYMPT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYMPT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
